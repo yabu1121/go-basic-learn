ed702f0765cc4b1a3a5b31961de33d79ce5d98c1
yabu1121 <n.hayato0122@outlook.jp>
2026-01-05 22:12:08 +0900

    スライスの活用

--- 01_basics/06.go
+++ 01_basics/06.go
@@ -26,4 +26,24 @@ func main(){
 		fmt.Printf("arr2[%d] = %d\n", i, v)
 	}
 	
+
+	// スライスというのは可変長で変形できる
+	// len()で長さを、cap()で容量を求められる
+
+	var slice1 []int;
+	fmt.Printf("s:%v l:%d c:%d\n", slice1, len(slice1), cap(slice1));
+	// メソッドでいじるんじゃなくて再代入をする、
+	slice1 = append(slice1, 1);
+	fmt.Printf("s:%v l:%d c:%d\n", slice1, len(slice1), cap(slice1));
+	// len(slice1) 長さを特定
+	// slice1[len(slice1)-1] 要素にアクセス
+	slice2 := []int{1,2,3,4,5};
+	fmt.Printf("s:%v l:%d c:%d\n", slice2, len(slice2), cap(slice2));
+
+	// スライスをmakeで作成できるようになる。
+	slice3 := make([]int, 3);
+	fmt.Printf("s:%v l:%d c:%d\n", slice3, len(slice3), cap(slice3));
+
+	slice4 := make([]int, 3,5);
+	fmt.Printf("s:%v l:%d c:%d\n", slice4, len(slice4), cap(slice4));
 }
