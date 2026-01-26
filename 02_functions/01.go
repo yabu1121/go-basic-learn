37fce5f6a81680c1319095d953d67d22ba5d01ae
yabu1121 <n.hayato0122@outlook.jp>
2026-01-26 23:44:31 +0900

    02-01

--- 02_functions/01.go
+++ 02_functions/01.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+func greet(){
+	fmt.Println("hello")
+}
+
+func taro(){
+	var taro string = "太郎"
+	var old int = 23
+	fmt.Printf("%s is %d years old.\n", taro, old)
+}
+
+func ReturnFunction () int {
+	return 20
+}
+
+func calc_num (a, b int) (int, int ){
+	sum := a + b
+	diff := a - b
+	return sum, diff
+}
+
 func main() {
-	fmt.Println("a")
+	greet()
+	taro()
+
+
+	var return_number int = ReturnFunction() 
+	fmt.Println(return_number)
+
+	var a int = 5
+	var b int = 2
+	var sum, diff int = calc_num(a,b)
+	fmt.Println(sum)
+	fmt.Println(diff)
 }
