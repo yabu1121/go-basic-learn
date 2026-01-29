454ae3412a7515a31c51ed4a6c3b2bd6ac070e3b
yabu1121 <n.hayato0122@outlook.jp>
2026-01-29 19:59:31 +0900

    users get

--- 05_web_basics/01.go
+++ 05_web_basics/01.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,10 +13,12 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/time", timeHandler)
+	http.HandleFunc("/api/user", userHandler)
+	http.HandleFunc("/api/users", userHandler)
 
 	// logライブラリを用いてport8081番を用いてhttpリクエストを待ち受ける。
 	// 第一引数、ポートを指定、第二引数nilを渡すとhttp.DefaultServeMuxを使用する。
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func homeHandler (w http.ResponseWriter, r *http.Request) {
@@ -39,4 +42,44 @@ func helloHandler (w http.ResponseWriter, r * http.Request){
 func timeHandler (w http.ResponseWriter, r *http.Request) {
 	now := time.Now().Format("2006-01-02 15:04:05")
 	fmt.Fprintf(w, "Current Time: %s", now)
+}
+
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func userHandler (w http.ResponseWriter, r *http.Request) {
+	user := User{
+		ID: 1, 
+		Name: "太郎",
+		Email: "taro@example.com",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+type Response struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func usersHandler (w http.ResponseWriter, r *http.Request) {
+	users := []User{
+		{ ID: 1, Name: "太郎", Email: "taro@example.com",},
+		{ ID: 2, Name: "次郎", Email: "jiro@example.com",},
+		{ ID: 3, Name: "三郎", Email: "saburo@example.com",},
+	}
+
+	resp := Response{
+		Success: true,
+		Message: "ユーザー一覧取得成功",
+		Data:    users,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
